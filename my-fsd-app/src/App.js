2138bb0c8f3ccaeb8874e106a73e5ae1f639784a
Bhuvan211 <BbhuvanBCA24@ced.alliance.edu.in>
2026-02-26 10:03:32 +0530

    Fixed submodule problem

---
+++ my-fsd-app/src/App.js
@@ -0,0 +1,64 @@
+import  React,{useState} from 'react';
+import "./styles.css";
+
+// export default function Head()
+// {
+//   return<div>
+//     <h1>Employee Details</h1>
+//     <p>Employee ID: 001</p>
+//     <p>Employee Name: Bhuvan B Krishna</p>
+//     <p>Employee Comapany: Epic Games</p>
+//     <p>Employee Salary: 25 LPA</p>
+//     <p>Employee Location: Bangalore</p>
+//     <p>Employee Phone_Numer: +91 8217347439</p>
+//     <iframe src='https://youtu.be/WqsI8wY42sM?si=AmgZpuQifA29wJ7S'></iframe>
+//   </div>
+// };
+// export default Head;
+
+
+
+
+// function Parent(props)
+// {
+//   return <p>Hello,I am {props.name} Studying in Bca 4th Sem Game Development  </p>
+// }
+// export default Child;
+// function Child()
+// {
+//   return(
+//     <Parent  name="Bhuvan B Krishna" />//JSX= JavascriptXML
+//   );
+// }
+
+
+// function State()
+// {
+//   const[message,setMessage] = useState("Hello");
+//   return<div>
+//     <h1>{message}</h1>
+//     <button
+//     onClick={()=>setMessage("Bhuvan")}
+//     >Change Text</button>
+//   </div>
+// }
+// export default State;
+
+// function Counter()
+// {
+//   const [count,setCount] = useState(0);
+//   return <div>
+//     <h1>Counter</h1>
+//     <p>{count}</p>
+//     <button
+//     onClick={()=>setCount(count-1)}
+//     >Decrement</button>
+//     <button
+//     onClick={()=>setCount(0)}
+//     >Zero</button>
+//     <button
+//     onClick={()=>setCount(count+1)}
+//     >Increment</button>
+//   </div>
+// }
+// export default Counter;
---
+++ youtube-login-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Main()
+{
+  return <div>
+    <h1>Youtube</h1>
+    <div id='cam'>📷</div>
+    <div id='username'>
+      <span>👤</span>
+      <input></input>
+    </div>
+  </div>
+}
+export default Main;
